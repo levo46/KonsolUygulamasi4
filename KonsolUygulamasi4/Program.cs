e2278a48c0945eda19bd9238160a445bac39ac6d
LEVENT YANIK <LEVENT YANIK@LEVENT>
2023-10-31 10:57:50 +0300

    Proje dosyası ekle.

---
+++ KonsolUygulamasi4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolUygulamasi4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dünya");
+            Console.Write("Levent");
+            Console.WriteLine(" Yanık");
+        }
+    }
+}
